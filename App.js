2c7bd17627c47c9c77dec1cc93bc46037af9d04d
Christy386 <christyo497@gmail.com>
2024-03-12 12:01:08 -0300

    foi colocado a screen details na pasta de screen

--- App.js
+++ App.js
@@ -1,18 +1,11 @@
 //import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+//import { StyleSheet, Text, View, Button } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import HomeScreen from './screens/HomeScreen'
-
-function DetailsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
+import DetailsScreen from './screens/DetailsScreen';
 
 
 const Stack = createNativeStackNavigator();
---
+++ screens/DetailsScreen/index.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function DetailsScreen() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
