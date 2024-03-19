0b5f1203832c9839ecff1ff0d5222d506e1ae821
Christy386 <christyo497@gmail.com>
2024-03-19 14:02:08 -0300

    leo tem q concertar umas coisas

--- screens/LoginScreen/index.js
+++ screens/LoginScreen/index.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import { View, TextInput, TouchableOpacity, CheckBox, Text, Image, Animated } from 'react-native';
+import { View, TextInput, TouchableOpacity, Text, Image, Animated } from 'react-native';
+import Checkbox from 'expo-checkbox';
 import { FontAwesome } from '@expo/vector-icons';
-import Logo from "../../assets/Logo.svg";
+
+import Logo from './../../assets/Logo.svg'
 import { styles } from './Styles';
 
 export default function LoginScreen({ navigation }) {
@@ -54,26 +56,40 @@ export default function LoginScreen({ navigation }) {
     <View style={styles.container}>
       <View style={styles.FormContainer}>
         <View style={styles.logoContainer}>
-          <Image source={Logo} style={styles.logo} />
+          <Logo style={styles.logo} />
         </View>
         <View style={styles.inputContainer}>
-          <TextInput placeholder={showIcon ? "" : "Username"} value={username} onChangeText={handleInputChange} style={styles.input} onFocus={handleInputFocus} onBlur={handleInputBlur}/>
+          <TextInput 
+            placeholder={showIcon ? "" : "Username"} 
+            value={username} 
+            onChangeText={handleInputChange} 
+            style={styles.input} 
+            onFocus={handleInputFocus} 
+            onBlur={handleInputBlur}
+          />
           {showIcon && (
-            <Animated.View style={[styles.icon, { opacity: iconAnimation, transform: [{ translateX: iconAnimation.interpolate({ inputRange: [0, 1], outputRange: [50, 0] }) }] }]}>
+            <Animated.View 
+              style={[styles.icon, { 
+                opacity: iconAnimation, 
+                transform: [{ translateX: iconAnimation.interpolate({ inputRange: [0, 1], outputRange: [50, 0] }) }] 
+              }]}>
               <FontAwesome name="user-circle" size={24} color="#9e9e9e" />
             </Animated.View>
           )}
         </View>
         <TextInput placeholder="Password" secureTextEntry={true} value={password} onChangeText={setPassword} style={styles.input} />
         <View style={styles.checkboxContainer}>
-          <CheckBox value={rememberMe} onValueChange={setRememberMe} tintColor="red"/>
+          <Checkbox value={rememberMe} onValueChange={setRememberMe} tintColor="red"/>
           <Text style={styles.checkboxText}>Keep me logged in</Text>
         </View>
         <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
           <Text style={{ color: '#fff', textAlign: 'center', fontSize:18, }}>Login</Text>
         </TouchableOpacity>
       </View>
-      <Text style={styles.registerText}>No account yet? <Text onPress={handleRegister} style={{color:"#00f", }}>Register here.</Text></Text>
+      <Text style={styles.registerText}>
+        No account yet? 
+        <Text onPress={handleRegister} style={{color:"#00f", }}>Register here.</Text>
+      </Text>
     </View>
   );
 }
