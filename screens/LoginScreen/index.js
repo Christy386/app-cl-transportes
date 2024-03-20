fdfc7564806899ec5c71b2f98d9c31c6be96bf2d
Leo Pinheiro <53160521+Stuka132@users.noreply.github.com>
2024-03-20 13:52:14 -0300

    Add files via upload

---
+++ screens/LoginScreen/index.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import { View, TextInput, TouchableOpacity, Text, Image, Animated } from 'react-native';
+import Checkbox from 'expo-checkbox';
+import { FontAwesome } from '@expo/vector-icons';
+
+import Logo from './../../assets/Logo.svg'
+import { styles } from './Styles';
+
+export default function LoginScreen({ navigation }) {
+  const [showIcon, setShowIcon] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
+  const [iconAnimation] = useState(new Animated.Value(0));
+
+  const handleLogin = () => {
+    if (username === 'client' && password === 'client'){
+      navigation.navigate('Home');
+    } else if(username === 'admin' && password === 'admin'){
+      //navigation.navigate('Admin');
+    } else {
+      return;
+    }
+    console.log('Remember Me:', rememberMe);
+  };
+
+  const handleRegister = ()=>{
+    console.log("Registrar");
+    //navigation.navigate('Register');
+  };
+
+  const handleInputFocus = () => {
+    setShowIcon(true);
+    startIconAnimation();
+  };
+
+  const handleInputBlur = () => {
+    if (!username) {
+      setShowIcon(false);
+    }
+  };
+
+  const handleInputChange = (text) => {
+    setUsername(text);
+  };
+
+  const startIconAnimation = () => {
+    Animated.timing(iconAnimation, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: false
+    }).start();
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.FormContainer}>
+        <View style={styles.logoContainer}>
+          <Logo style={styles.logo} />
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput 
+            placeholder={showIcon ? "" : "Username"} 
+            value={username} 
+            onChangeText={handleInputChange} 
+            style={styles.input} 
+            onFocus={handleInputFocus} 
+            onBlur={handleInputBlur}
+          />
+          {showIcon && (
+            <Animated.View 
+              style={[styles.icon, { 
+                opacity: iconAnimation, 
+                transform: [{ translateX: iconAnimation.interpolate({ inputRange: [0, 1], outputRange: [50, 0] }) }] 
+              }]}>
+              <FontAwesome name="user-circle" size={24} color="#9e9e9e" />
+            </Animated.View>
+          )}
+        </View>
+        <TextInput placeholder="Password" secureTextEntry={true} value={password} onChangeText={setPassword} style={styles.input} />
+        <View style={styles.checkboxContainer}>
+          <Checkbox value={rememberMe} onValueChange={setRememberMe} tintColor="red"/>
+          <Text style={styles.checkboxText}>Keep me logged in</Text>
+        </View>
+        <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+          <Text style={{ color: '#fff', textAlign: 'center', fontSize:18, }}>Login</Text>
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.registerText}>
+        <Text>No account yet?</Text>
+        <Text onPress={handleRegister} style={{color:"#00f", }}>Register here.</Text>
+      </Text>
+    </View>
+  );
+}
